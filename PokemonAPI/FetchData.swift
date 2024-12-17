1049b6cd3a84196f1a999c59bea02781420443d0
Edison Zheng <eddiezheng2678@gmail.com>
2024-12-17 15:08:16 -0500

    copied pasted

--- PokemonAPI/FetchData.swift
+++ PokemonAPI/FetchData.swift
@@ -8,5 +8,11 @@
 import Foundation
 
 struct FetchData {
-    
+    mutating func getData() async {
+        var URLString = "https://newsapi.org/v2/everything?q=roblox&language=en&sortBy=publishedAt&apiKey=91d509dcf2e34ebd96eb0525be6751bd"
+        
+        guard let url = URL(string: URLString) else {return}
+        guard let (data, _) = try? await URLSession.shared.data(from: url) else {return}
+        
+    }
 }
