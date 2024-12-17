dc0cca6ce6d84ffcb0f82d143c71f005d90158af
Edison Zheng <eddiezheng2678@gmail.com>
2024-12-17 14:47:52 -0500

    Initial Commit

---
+++ PokemonAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonAPI
+//
+//  Created by Edison Zheng (student LM) on 12/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonAPI/PokemonAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAPIApp.swift
+//  PokemonAPI
+//
+//  Created by Edison Zheng (student LM) on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
