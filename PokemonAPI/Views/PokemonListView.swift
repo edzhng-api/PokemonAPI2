8ac769fbebfa10d13568ed8c29dfe3c533148c9f
Filip <cyranfilip.cf@gmail.com>
2025-01-08 11:04:07 -0500

    Update PokemonListView.swift

--- PokemonAPI/Views/PokemonListView.swift
+++ PokemonAPI/Views/PokemonListView.swift
@@ -10,47 +10,64 @@ import SwiftUI
 struct PokemonListView: View {
     @State var data = FetchData()
     @State var pokemonData: [String: FetchPokemonData] = [:]
-    
+
     var body: some View {
-        ScrollView{
-            ForEach(data.response.results){ pokemon in
-                VStack{
-                    Text(pokemon.name ?? " ")
-                        .font(Constants.textFont)
-                    
-                    if let pokemonData = pokemonData[pokemon.name ?? ""]{
-                        Text("Weight: \(pokemonData.response.weight)")
-                            .font(Constants.textFont)
+        NavigationView {
+            List(data.response.results, id: \.name) { pokemon in
+                NavigationLink(destination: PokemonView(pokemonName: pokemon.name ?? "",photoURL: pokemonData[pokemon.name ?? ""]?.response.sprites.front_default)) {
+                    if let fetchedData = pokemonData[pokemon.name ?? ""] {
+                        if let imageURL = URL(string: fetchedData.response.sprites.front_default ?? "") {
+                            AsyncImage(url: imageURL) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                            } placeholder: {
+                                ProgressView()
+                            }
+                        }
                     } else {
-                        Text("Loading...")
+                        Text("Loading details...")
+                            .font(Constants.textFont)
+                            .foregroundColor(.gray)
                             .onAppear {
                                 Task {
                                     await fetchPokemonData(for: pokemon)
                                 }
                             }
                     }
-                }
                     
+                    Text("\(pokemon.name ?? "")")
+                        .font(Constants.textFont)
+                }
             }
-        }.onAppear {
-            Task {
-                await fetchPokemonData()
+            .navigationTitle("Pokemon List")
+            .onAppear {
+                Task {
+                    await fetchPokemonData()
+                }
             }
         }
     }
+
     private func fetchPokemonData() async {
         await data.getData()
     }
-    
     private func fetchPokemonData(for pokemon: Pokemon) async {
-        if let pokemonURL = pokemon.url {
-            var fetcher = FetchPokemonData(PokemonURL: pokemonURL)
-            await fetcher.getData()
-            
-            pokemonData[pokemon.name ?? ""] = fetcher
+        guard let pokemonName = pokemon.name, let pokemonURL = pokemon.url else { return }
+
+        if pokemonData[pokemonName] != nil { return }
+        
+        var fetcher = FetchPokemonData(PokemonURL: pokemonURL.absoluteString)
+        await fetcher.getData()
+
+        DispatchQueue.main.async {
+            pokemonData[pokemonName] = fetcher
         }
     }
+
 }
+
 
 #Preview {
     PokemonListView()
