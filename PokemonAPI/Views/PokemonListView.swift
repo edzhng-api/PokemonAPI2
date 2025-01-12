04201dc6303eeaf479fc0416b1067fe533fc6592
Sebastian Steed <seb.steed@gmail.com>
2025-01-12 13:28:05 -0500

    Added search bar but its janky

--- PokemonAPI/Views/PokemonListView.swift
+++ PokemonAPI/Views/PokemonListView.swift
@@ -10,66 +10,167 @@ import SwiftUI
 struct PokemonListView: View {
     @State var data = FetchData()
     @State var pokemonData: [String: FetchPokemonData] = [:]
+    @State var searchResults: [Pokemon] = []
+    @State var searchQuery: String = ""
+    
+    var isSearching: Bool {
+        return !searchQuery.isEmpty
+    }
 
     var body: some View {
         NavigationView {
-            List(data.response.results, id: \.name) { pokemon in
-                NavigationLink(destination: {
-                    if let fetchedData = pokemonData[pokemon.name ?? ""] {
-                        PokemonView(
-                            pokemonName: pokemon.name ?? "",
-                            photoURL: fetchedData.response.sprites.front_default,
-                            homePhotoURL: fetchedData.response.sprites.other?.home?.front_default,
-                            types: fetchedData.response.types.map { $0.type.name ?? "Unknown" },
-                            abilities: fetchedData.response.abilities.map { $0.ability.name ?? "Unknown Ability" }, 
-                            height: fetchedData.response.height,
-                            weight: fetchedData.response.weight)
-                    } else {
-                        Text("Loading...")
-                    }
-                }) {
-                    if let fetchedData = pokemonData[pokemon.name ?? ""] {
-                        if let imageURL = URL(string: fetchedData.response.sprites.front_default ?? "") {
-                            AsyncImage(url: imageURL) { image in
-                                image
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                            } placeholder: {
-                                ProgressView()
-                            }
+            if isSearching {
+                List(searchResults) { pokemon in
+                    NavigationLink(destination: {
+                        if let fetchedData = pokemonData[pokemon.name ?? ""] {
+                            PokemonView(
+                                pokemonName: pokemon.name ?? "",
+                                photoURL: fetchedData.response.sprites.front_default,
+                                homePhotoURL: fetchedData.response.sprites.other?.home?.front_default,
+                                types: fetchedData.response.types.map { $0.type.name ?? "Unknown" },
+                                abilities: fetchedData.response.abilities.map { $0.ability.name ?? "Unknown Ability" },
+                                height: fetchedData.response.height,
+                                weight: fetchedData.response.weight)
+                        } else {
+                            Text("Loading...")
                         }
-                        VStack {
-                            HStack {
-                                Text("\(pokemon.name ?? "")")
-                                    .font(Constants.textFont)
-                                Spacer()
+                    }) {
+                        if let fetchedData = pokemonData[pokemon.name ?? ""] {
+                            if let imageURL = URL(string: fetchedData.response.sprites.front_default ?? "") {
+                                AsyncImage(url: imageURL) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                } placeholder: {
+                                    ProgressView()
+                                }
                             }
-                            ForEach(fetchedData.response.types, id: \.slot) { type in
+                            VStack {
                                 HStack {
-                                    Text(type.type.name ?? "Unknown Type")
-                                        .font(.caption)
-                                    .foregroundColor(.gray)
+                                    Text("\(pokemon.name ?? "")")
+                                        .font(Constants.textFont)
                                     Spacer()
                                 }
-                            }
-                        }
-                    } else {
-                        Text("Loading details...")
-                            .font(Constants.textFont)
-                            .foregroundColor(.gray)
-                            .onAppear {
-                                Task {
-                                    await fetchPokemonData(for: pokemon)
+                                ForEach(fetchedData.response.types, id: \.slot) { type in
+                                    HStack {
+                                        Text(type.type.name ?? "Unknown Type")
+                                            .font(.caption)
+                                            .foregroundColor(.gray)
+                                        Spacer()
+                                    }
                                 }
                             }
+                        } else {
+                            Text("Loading details...")
+                                .font(Constants.textFont)
+                                .foregroundColor(.gray)
+                                .onAppear {
+                                    Task {
+                                        await fetchPokemonData(for: pokemon)
+                                    }
+                                }
+                        }
+                    }
+                }
+                .navigationTitle("Pokemon List")
+                .onAppear {
+                    Task {
+                        await fetchPokemonData()
+                    }
+                }
+                .searchable(text: $searchQuery, placement:.automatic, prompt: "Pokemon Name")
+                .textInputAutocapitalization(.never)
+                
+                .onChange(of: searchQuery) {
+                    self.fetchSearchResults(for: searchQuery)
+                }
+                            
+                .overlay {
+                    if isSearching && searchResults.isEmpty {
+                        ContentUnavailableView(
+                            "Product not available",
+                            systemImage: "magnifyingglass",
+                            description: Text("No results for **\\(search)**")
+                        )
                     }
                 }
             }
-            .navigationTitle("Pokemon List")
-            .onAppear {
-                Task {
-                    await fetchPokemonData()
+            else {
+                List(data.response.results, id: \.name) { pokemon in
+                    NavigationLink(destination: {
+                        if let fetchedData = pokemonData[pokemon.name ?? ""] {
+                            PokemonView(
+                                pokemonName: pokemon.name ?? "",
+                                photoURL: fetchedData.response.sprites.front_default,
+                                homePhotoURL: fetchedData.response.sprites.other?.home?.front_default,
+                                types: fetchedData.response.types.map { $0.type.name ?? "Unknown" },
+                                abilities: fetchedData.response.abilities.map { $0.ability.name ?? "Unknown Ability" },
+                                height: fetchedData.response.height,
+                                weight: fetchedData.response.weight)
+                        } else {
+                            Text("Loading...")
+                        }
+                    }) {
+                        if let fetchedData = pokemonData[pokemon.name ?? ""] {
+                            if let imageURL = URL(string: fetchedData.response.sprites.front_default ?? "") {
+                                AsyncImage(url: imageURL) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                            }
+                            VStack {
+                                HStack {
+                                    Text("\(pokemon.name ?? "")")
+                                        .font(Constants.textFont)
+                                    Spacer()
+                                }
+                                ForEach(fetchedData.response.types, id: \.slot) { type in
+                                    HStack {
+                                        Text(type.type.name ?? "Unknown Type")
+                                            .font(.caption)
+                                            .foregroundColor(.gray)
+                                        Spacer()
+                                    }
+                                }
+                            }
+                        } else {
+                            Text("Loading details...")
+                                .font(Constants.textFont)
+                                .foregroundColor(.gray)
+                                .onAppear {
+                                    Task {
+                                        await fetchPokemonData(for: pokemon)
+                                    }
+                                }
+                        }
+                    }
+                }
+                .navigationTitle("Pokemon List")
+                .onAppear {
+                    Task {
+                        await fetchPokemonData()
+                    }
+                }
+                .searchable(text: $searchQuery, placement:.automatic, prompt: "Pokemon Name")
+                .textInputAutocapitalization(.never)
+                
+                .onChange(of: searchQuery) {
+                    self.fetchSearchResults(for: searchQuery)
+                }
+                            
+                .overlay {
+                    if isSearching && searchResults.isEmpty {
+                        ContentUnavailableView(
+                            "Product not available",
+                            systemImage: "magnifyingglass",
+                            description: Text("No results for **\\(searchQuery)**")
+                        )
+                    }
                 }
             }
         }
@@ -88,6 +189,13 @@ struct PokemonListView: View {
 
         DispatchQueue.main.async {
             pokemonData[pokemonName] = fetcher
+        }
+    }
+    private func fetchSearchResults(for query: String) {
+        searchResults = data.response.results.filter { pokemon in
+            pokemon.name!
+                .lowercased()
+                .contains(searchQuery)
         }
     }
 }
